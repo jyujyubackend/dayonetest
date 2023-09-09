9e8f6a94f7a7ec59d5fd12b31018374ab1b23619
jyujyubackend <jyujyubackend@gmail.com>
2023-09-10 02:14:27 +0900

    KafkaIntegrationTest 보완

--- src/test/java/com/jyujyu/dayonetest/KafkaConsumerApplicationTests.java
+++ src/test/java/com/jyujyu/dayonetest/KafkaConsumerApplicationTests.java
@@ -3,13 +3,16 @@ package com.jyujyu.dayonetest;
 import com.jyujyu.dayonetest.service.KafkaConsumerService;
 import com.jyujyu.dayonetest.service.KafkaProducerService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+@Order(0)
 public class KafkaConsumerApplicationTests extends IntegrationTest {
+
     @Autowired
     private KafkaProducerService kafkaProducerService;
 
